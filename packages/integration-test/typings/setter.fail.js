d3e3aa8c52975d7d3acdf9cb5c9aaad8b3eca167
William Wong <compulim@users.noreply.github.com>
2025-12-30 09:55:11 +0000

    Remove emitted file

--- packages/integration-test/typings/setter.fail.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var use_ref_from_1 = require("use-ref-from");
-// eslint-disable-next-line react-hooks/rules-of-hooks
-(0, use_ref_from_1.useRefFrom)(123).current = 234;
