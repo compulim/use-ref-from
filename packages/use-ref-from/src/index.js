fdf24793d62160aa9c228c056de66524f564f498
William Wong <compulim@users.noreply.github.com>
2025-12-30 11:12:52 +0000

    Move typing test transformer to test-harness

---
+++ packages/use-ref-from/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useRefFrom = void 0;
+var useRefFrom_ts_1 = require("./useRefFrom.ts");
+exports.useRefFrom = useRefFrom_ts_1.default;
