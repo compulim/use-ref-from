cc40c4e6e558752703e745ca23a002d796a7342c
William Wong <compulim@users.noreply.github.com>
2025-12-30 23:01:26 +0000

    Clean up

--- packages/integration-test/src/setter.test-types.ts
+++ packages/integration-test/src/setter.test-types.ts
@@ -1,4 +1,4 @@
-import { useRefFrom } from '../index.ts';
+import { useRefFrom } from 'use-ref-from';
 
 // @ts-expect-error Cannot assign to 'current' because it is a read-only property.
 useRefFrom<number>(123).current = 234;
